40ad7411b8698b2694b2de59f8872a02058cd4e2
Alexis Guzman <alexisguzman157@outlook.com>
2021-08-27 23:26:01 -0400

    Added hours and time blocks., changed the description current hours color, still need to have it activate with the current hour.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+
+function timeTracker() {
+
+    var currentHour = moment().hour();
+
